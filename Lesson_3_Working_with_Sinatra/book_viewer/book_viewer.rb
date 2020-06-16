e760de58d2b6512e954b5da79474d1bbcab032a6
Callie Buruchara <callieburuchara@gmail.com>
2020-06-16 17:20:10 +0300

    Completed basic search for book viewer

--- Lesson_3_Working_with_Sinatra/book_viewer/book_viewer.rb
+++ Lesson_3_Working_with_Sinatra/book_viewer/book_viewer.rb
@@ -34,3 +34,29 @@ get '/chapters/:number' do
 
   erb :chapter
 end
+
+def each_chapter
+  @table_of_contents.each_with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+def chapters_matching(query)
+  results = []
+
+  return results if !query || query.empty?
+
+  each_chapter do |number, name, contents|
+    results << {number: number, name: name} if contents.include?(query)
+  end
+  
+  results
+end
+
+get '/search' do
+  @results = chapters_matching(params[:query])
+  erb :search
+end
+
